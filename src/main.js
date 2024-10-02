780e93f41e6d855999d8f008a84de72e8e2ec325
JeanCarlos Jimenez <jeancarlosjimenezc25@gmail.com>
2024-10-02 06:47:40 -0500

    Changes Files - Vanilla.js

--- src/main.js
+++ src/main.js
@@ -1,9 +1,9 @@
-import './styles/style.css'
-import js from './assets/js.svg'
-import vite from '/vite.svg'
-import { setupCounter } from './components/counter.js'
+import "./styles/style.css";
+import js from "./assets/js.svg";
+import vite from "/vite.svg";
+import { setupCounter } from "./components/counter.js";
 
-document.querySelector('#app').innerHTML = `
+document.querySelector("#app").innerHTML = `
       <div id="app"
         class="m-5 p-5 flex flex-col justify-center items-center" >
         <div id="app-container"
@@ -12,25 +12,25 @@ document.querySelector('#app').innerHTML = `
             Vanilla.js
           </h1>
           <a href="https://vitejs.dev" target="_blank">
-          <img src="${vite}" class="logo-vite" alt="Logo-Vite" />
+          <img src="${vite}" id="logo-vite" alt="Logo-Vite" />
         </a>
         <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-          <img src="${js}" class="logo-js" alt="Logo-Js" />
+          <img src="${js}" id="logo-js" class="rounded-2xl" alt="Logo-Js" />
         </a>
         </div>
         <div id="app-card"
-          class="m-2.5 p-2.5 flex flex-row justify-center items-center" >
+          class="m-1 p-1 flex text-[#333] flex-row justify-center items-center" >
           <div class="card">
-            <button id="counter" type="button"></button>
+            <button class="m-2.5 p-2.5 bg-[#c2bc8f]" id="counter" type="button"></button>
           </div>
-          <p class="m-2.5 p-2.5 bg-[#1A1A21] text-slate-300 rounded-xl" >
+          <p class="m-2.5 p-2.5 bg-[#ddc583] text-[#333] rounded-xl" >
             Edit <code class="code">./src/main.js</code> and save to test Hmr
           </p>
         </div>
-        <p id="read" class="m-2.5 p-2.5 bg-[#1A121A] rounded-lg"  >
+        <p id="read" class="m-2.5 p-2.5 bg-[#d0d455] text-[#333] rounded-lg"  >
           Click on the Vite and React Logos to learn more
         </p>
       </div>
-`
+`;
 
-setupCounter(document.querySelector('#counter'))
+setupCounter(document.querySelector("#counter"));
