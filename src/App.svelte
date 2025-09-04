8cbc45afec3f2cf99312471a07d012bf9616d293
JeanCarlos Jimenez <jeancarlosjimenezc25@gmail.com>
2025-09-04 12:26:02 -0500

    Changes Files - Main

--- src/App.svelte
+++ src/App.svelte
@@ -13,17 +13,16 @@
       <a href="/about">About</a>
     </nav>
     <section>
-      <p>
-        Welcome to News-Jct, your go-to source for the latest news updates.
-      </p>
+      <p>Welcome to News-Jct, your go-to source for the latest news updates.</p>
     </section>
     <section>
       <h2>News</h2>
+      <p>Stay tuned for the latest news articles and updates.</p>
       <ul>
         <ol>Numero uno</ol>
-        <li>
+        <ol>
           <a href="http://">News item 1</a>
-        </li>
+        </ol>
       </ul>
     </section>
     <Counter />
